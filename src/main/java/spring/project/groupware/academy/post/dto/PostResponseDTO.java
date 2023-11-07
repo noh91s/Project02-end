ba7272d245ae0392b2c61bf24affc2bd6dafc3ce
noh91 <nonsdrdr@gmail.com>
2023-11-07 15:54:31 +0900

    readme

--- src/main/java/spring/project/groupware/academy/post/dto/PostResponseDTO.java
+++ src/main/java/spring/project/groupware/academy/post/dto/PostResponseDTO.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import spring.project.groupware.academy.board.entity.NoticeEntity;
 import spring.project.groupware.academy.post.entity.Post;
 
 import java.util.Optional;
